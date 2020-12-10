6c721aa89fe2e5d3f119158fc04995f697323d1b
Desaxion <58693063+Desaxion@users.noreply.github.com>
2020-12-10 18:26:20 +0100

    Release V.1

---
+++ docs/precache-manifest.09e011057a021d7cfa6916cb2e940f1d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eebb2d5ff61e1a22346b15f25a9fa34",
+    "url": "/klapp/index.html"
+  },
+  {
+    "revision": "b12b79f2d97847bdfd7d",
+    "url": "/klapp/static/css/main.56018af2.chunk.css"
+  },
+  {
+    "revision": "e81b591d32a7a4e921c2",
+    "url": "/klapp/static/js/2.a386dcc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/klapp/static/js/2.a386dcc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12b79f2d97847bdfd7d",
+    "url": "/klapp/static/js/main.c135a098.chunk.js"
+  },
+  {
+    "revision": "4616a2cd7f5c61fcab23",
+    "url": "/klapp/static/js/runtime-main.815e4e38.js"
+  },
+  {
+    "revision": "e5f6f161895a609d570203efae455c53",
+    "url": "/klapp/static/media/Budget.e5f6f161.svg"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/klapp/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "91dafe84020e57a1d15dc0e8ee17a52a",
+    "url": "/klapp/static/media/Marons-Regular.91dafe84.ttf"
+  },
+  {
+    "revision": "36b5bab58a18b9c924861a4ccbf1a790",
+    "url": "/klapp/static/media/Roboto-Bold.36b5bab5.ttf"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/klapp/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "27e0ad19de21e9a21fd1786ae100ca7f",
+    "url": "/klapp/static/media/add (1).27e0ad19.svg"
+  },
+  {
+    "revision": "8932d3900cf7b41c2a0660ee16fd12e5",
+    "url": "/klapp/static/media/arrow.8932d390.svg"
+  },
+  {
+    "revision": "eeeecba8c092bf42865eb322c2c11f6e",
+    "url": "/klapp/static/media/check.eeeecba8.svg"
+  },
+  {
+    "revision": "5081e6000924a404f5da63159cab10c5",
+    "url": "/klapp/static/media/christmas-sack.5081e600.svg"
+  },
+  {
+    "revision": "576572175a419a64c1f391724c22395d",
+    "url": "/klapp/static/media/delete.57657217.svg"
+  },
+  {
+    "revision": "6724956971d53901e32d6451b8c3ec9b",
+    "url": "/klapp/static/media/edit (1).67249569.svg"
+  },
+  {
+    "revision": "5bf174d8fc0bd452ab9510e37939439f",
+    "url": "/klapp/static/media/listwhite.5bf174d8.svg"
+  },
+  {
+    "revision": "371be4d53ada7a955b0c95a14c4f095b",
+    "url": "/klapp/static/media/man-user (1).371be4d5.svg"
+  },
+  {
+    "revision": "bcdf7d71e3bb27eae94e90b83c1af898",
+    "url": "/klapp/static/media/nocheck.bcdf7d71.svg"
+  }
+]);
